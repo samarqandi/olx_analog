3e56490370c7d203f9094df3b9eb5d0fd8be15ed
Muhammad Yahyo <san4o.original@gamil.com>
2022-06-07 03:35:35 +0500

    added new packages

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:olx_analog/bottom_nav_bar.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,35 +13,35 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         primarySwatch: Colors.teal,
       ),
-      title: 'Material App',
+      title: 'OLX analog',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-            unselectedItemColor: Colors.black26,
-            selectedItemColor: Colors.black54,
-            backgroundColor: Colors.amberAccent,
-            items: bottomNavBar),
-        appBar: AppBar(
-          title: Container(
-            width: double.infinity,
-            height: 40,
-            color: Colors.white,
-            child: const Center(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search),
+          bottomNavigationBar: BottomNavigationBar(
+              unselectedItemColor: Colors.black26,
+              selectedItemColor: Colors.black54,
+              backgroundColor: Colors.amberAccent,
+              items: bottomNavBar),
+          appBar: AppBar(
+            title: Container(
+              width: double.infinity,
+              height: 40,
+              color: Colors.white,
+              child: const Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        body: Center(
-          child: Container(
-            child: const Text('Hello World'),
-          ),
-        ),
-      ),
+          body: Center(
+            child: CircleAvatar(
+              radius: 24.0,
+              backgroundImage: AssetImage("assets/icon/icon.png"),
+            ),
+          )),
     );
   }
 }
