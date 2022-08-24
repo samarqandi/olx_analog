f0706b8487f61ac14b989b7f3684bc8fd8b1d62d
samarqandi <102905058+samarqandi@users.noreply.github.com>
2022-08-24 05:14:45 +0500

    Delete lib/theme directory

--- lib/theme/app_bar_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const appBarTheme = AppBarTheme(
-  backgroundColor: Colors.greenAccent,
-  shadowColor: Colors.transparent,
-);
--- lib/theme/text_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const textTheme = TextTheme(
-  headline5: TextStyle(color: Colors.white),
-  bodyText2: TextStyle(color: Colors.white),
-);
